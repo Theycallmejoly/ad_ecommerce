4fa6b0694095b9c007a248450bce11c9a424703e
theycallmejoly <yking9187@gmail.com>
2024-08-10 22:30:19 +0330

    First Setups

---
+++ shop/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import index , checkout , product , store
+
+
+urlpatterns = [
+    path('index/' , index , name = 'index'),
+    path('store/' , store , name = 'store' ),
+    path('product/' ,  product , name = 'product' ),
+    path('checkout/' , checkout , name = 'checkout'),
+]
---
+++ shop/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render (request , "index.html")
+
+def store(request):
+    return render (request , "store.html")
+
+def product(request):
+    return render (request , "product.html")
+
+def checkout(request):
+    return render (request , "checkout.html")
